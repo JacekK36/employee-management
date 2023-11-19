37a49260c499347f7240189c6784a0cbb172a499
Jacek Kalinowski <jacekkalinowski27@gmail.com>
2023-11-19 09:07:35 +0100

    add NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import { Home } from "./components/Home";
 import { EmployeesList } from "./components/EmployeesList";
 import { EmployeeDetails } from "./components/EmployeeDetails";
 import { EmployeesProvider } from "./context/EmployeesContext";
+import { Navi } from "./components/Navi";
+import { ButtonGroup } from "./components/Navi";
 
 function App() {
   return (
     <EmployeesProvider>
       <div className="App">
+        <ButtonGroup />
+        <Navi />
         <Routes>
           <Route path="/" element={<Home />} />;
           <Route path="/employees" element={<EmployeesList />} />;
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,15 +1,25 @@
+import { useContext } from "react";
+import { EmployeesContext } from "../../context/EmployeesContext";
 import "./Home.scss";
+import { Button, ButtonGroup } from "@mui/material";
 
 export const Home = () => {
   const { employeesList } = useContext(EmployeesContext);
   return (
-    <div className="home">
-      <h1 className="home__title">Welcome to the employee-management App</h1>
-      <p className="home__employees">
-        Number of activ Employees: {employeesList.length}
-      </p>
-      <h4 className="homepage__text">Employees List</h4>
-      <button type="submit">Go to List</button>
-    </div>
+    <>
+      <div className="home">
+        <h1 className="home__title">Welcome to the employee-management App</h1>
+        <p className="home__employees">
+          Number of activ Employees: {employeesList.length}
+        </p>
+      </div>
+      <div className="ButtonGroup">
+        <ButtonGroup variant="text" aria-label="text button group">
+          <Button>Go to Employee List</Button>
+          <Button>Add Employee</Button>
+          <Button>Remove Employee</Button>
+        </ButtonGroup>
+      </div>
+    </>
   );
 };
