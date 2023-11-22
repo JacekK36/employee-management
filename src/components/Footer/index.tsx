5f2673d296572b05a78b36badbe14ae7a3a253bd
Natalia <natalia.tomasz.pilot@gmail.com>
2023-11-22 22:25:39 +0100

    Fixed bugs in responsive

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import "./Footer.scss";
 export const Footer = () => {
   return (
     <footer className="authors">
-      <p>
+      <p className="authors__text">
         Iryna Jacek Karol Krzysztof Natalia <br /> All rights reserved
       </p>
     </footer>
