8ae072b53753a82318dffa72753f1ace8ef00388
Natalia <natalia.tomasz.pilot@gmail.com>
2023-11-25 10:38:53 +0100

    Add loader to EmployeesList

--- src/components/EmployeesList/index.tsx
+++ src/components/EmployeesList/index.tsx
@@ -15,109 +15,116 @@ export const EmployeesList: React.FC = () => {
   return (
     <>
       <div className="employees-list">
-        {employeesList.length > 0 ? (
-          <div className="employees-list__table">
-            <h2>Employees list</h2>
-            <table className="table">
-              <thead className="thead-dark">
-                <tr>
-                  <th onClick={() => handleOrder("id")}>
-                    ID{" "}
-                    {sort === "id" && (
-                      <SortIcon
-                        className={
-                          order === "desc"
-                            ? "employees-list__sort--desc employees-list__sort"
-                            : "employees-list__sort--asc employees-list__sort"
-                        }
-                      />
-                    )}
-                  </th>
-                  <th onClick={() => handleOrder("firstName")}>
-                    Imię{" "}
-                    {sort === "firstName" && (
-                      <SortIcon
-                        className={
-                          order === "desc"
-                            ? "employees-list__sort--desc employees-list__sort"
-                            : "employees-list__sort--asc employees-list__sort"
-                        }
-                      />
-                    )}
-                  </th>
-                  <th onClick={() => handleOrder("lastName")}>
-                    Nazwisko{" "}
-                    {sort === "lastName" && (
-                      <SortIcon
-                        className={
-                          order === "desc"
-                            ? "employees-list__sort--desc employees-list__sort"
-                            : "employees-list__sort--asc employees-list__sort"
-                        }
-                      />
-                    )}
-                  </th>
-                  <th onClick={() => handleOrder("salary")}>
-                    Pensja{" "}
-                    {sort === "salary" && (
-                      <SortIcon
-                        className={
-                          order === "desc"
-                            ? "employees-list__sort--desc employees-list__sort"
-                            : "employees-list__sort--asc employees-list__sort"
-                        }
-                      />
-                    )}
-                  </th>
-                  <th onClick={() => handleOrder("status")}>
-                    Status{" "}
-                    {sort === "status" && (
-                      <SortIcon
-                        className={
-                          order === "desc"
-                            ? "employees-list__sort--desc employees-list__sort"
-                            : "employees-list__sort--asc employees-list__sort"
-                        }
-                      />
-                    )}
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {employeesList.map((employee) => (
-                  <tr
-                    key={employee.id}
-                    onClick={() =>
-                      navigate(
-                        `${location.pathname}/${employee.id}${location.search}`
-                      )
-                    }
-                  >
-                    <td>{employee.id}</td>
-                    <td>{employee.firstName}</td>
-                    <td>{employee.lastName}</td>
-                    <td>{employee.salary}</td>
-                    <td>{employee.status}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+        {"loaderEmployees" ? (
+          <p>Loading...</p>
         ) : (
-          <p>Brak pracowników</p>
+          <>
+            {employeesList.length > 0 ? (
+              <div className="employees-list__table">
+                <h2>Employees list</h2>
+                <table className="table">
+                  <thead className="thead-dark">
+                    <tr>
+                      <th onClick={() => handleOrder("id")}>
+                        ID{" "}
+                        {sort === "id" && (
+                          <SortIcon
+                            className={
+                              order === "desc"
+                                ? "employees-list__sort--desc employees-list__sort"
+                                : "employees-list__sort--asc employees-list__sort"
+                            }
+                          />
+                        )}
+                      </th>
+                      <th onClick={() => handleOrder("firstName")}>
+                        Imię{" "}
+                        {sort === "firstName" && (
+                          <SortIcon
+                            className={
+                              order === "desc"
+                                ? "employees-list__sort--desc employees-list__sort"
+                                : "employees-list__sort--asc employees-list__sort"
+                            }
+                          />
+                        )}
+                      </th>
+                      <th onClick={() => handleOrder("lastName")}>
+                        Nazwisko{" "}
+                        {sort === "lastName" && (
+                          <SortIcon
+                            className={
+                              order === "desc"
+                                ? "employees-list__sort--desc employees-list__sort"
+                                : "employees-list__sort--asc employees-list__sort"
+                            }
+                          />
+                        )}
+                      </th>
+                      <th onClick={() => handleOrder("salary")}>
+                        Pensja{" "}
+                        {sort === "salary" && (
+                          <SortIcon
+                            className={
+                              order === "desc"
+                                ? "employees-list__sort--desc employees-list__sort"
+                                : "employees-list__sort--asc employees-list__sort"
+                            }
+                          />
+                        )}
+                      </th>
+                      <th onClick={() => handleOrder("status")}>
+                        Status{" "}
+                        {sort === "status" && (
+                          <SortIcon
+                            className={
+                              order === "desc"
+                                ? "employees-list__sort--desc employees-list__sort"
+                                : "employees-list__sort--asc employees-list__sort"
+                            }
+                          />
+                        )}
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {employeesList.map((employee) => (
+                      <tr
+                        key={employee.id}
+                        onClick={() =>
+                          navigate(
+                            `${location.pathname}/${employee.id}${location.search}`
+                          )
+                        }
+                      >
+                        <td>{employee.id}</td>
+                        <td>{employee.firstName}</td>
+                        <td>{employee.lastName}</td>
+                        <td>{employee.salary}</td>
+                        <td>{employee.status}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              <p>Brak pracowników</p>
+            )}
+            <div className="btn">
+              <button onClick={() => handlePage(-1)} disabled={page === 1}>
+                Prev
+              </button>
+              <span>
+                {page} of {maxPage}
+              </span>
+              <button onClick={() => handlePage(1)} disabled={page === maxPage}>
+                Next
+              </button>
+            </div>
+          </>
         )}
-        <div className="btn">
-          <button onClick={() => handlePage(-1)} disabled={page === 1}>
-            Prev
-          </button>
-          <span>
-            {page} of {maxPage}
-          </span>
-          <button onClick={() => handlePage(1)} disabled={page === maxPage}>
-            Next
-          </button>
-        </div>
       </div>
+
       {id && <EmployeeDetails />}
     </>
   );
